2ebfc37f6e9a3aa272564460ea64c9c08db49031
zhangnan <zhangnan35@foxmail.com>
2021-09-27 18:04:48 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import { Tab, Tabs } from "vant";
 
 Vue.config.productionTip = false;
-
-// Vue.use(Tab).use(Tabs);
 
 new Vue({
   router,
